4983b71045b8086a9ac66890229a2ca3ae258fc0
Priyanshu <jashram826@gmail.com>
2025-01-10 15:52:55 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from './pages/Layout'
 import './App.css'
+import useLenis from './components/Lennis'
 import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar'
 import WorkPage from './pages/WorkPage'
@@ -9,6 +10,7 @@ import SkillsAndContact from './pages/SkillsPage'
 import Footer from './components/Footer'
 
 const App = () => {
+  useLenis();
   return (
     <div className='main-app'>
       <HomePage />
---
+++ src/components/Lennis.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+
+const useLenis = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    console.log('Lenis initialized:', lenis);
+
+    return () => {
+      lenis.destroy(); // Cleanup on unmount
+    };
+  }, []);
+};
+
+export default useLenis;
